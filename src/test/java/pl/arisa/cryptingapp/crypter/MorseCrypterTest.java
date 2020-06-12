023e69e3bfe50b712f74b831e78d2c11238f2f52
AFormela <aleksandra_formela@vp.pl>
2020-06-12 23:37:45 +0200

    Input repairs

--- src/test/java/pl/arisa/cryptingapp/crypter/MorseCrypterTest.java
+++ src/test/java/pl/arisa/cryptingapp/crypter/MorseCrypterTest.java
@@ -21,4 +21,20 @@ public class MorseCrypterTest {
         ICrypter crypter = new MorseCrypter();
         assertEquals(expected, crypter.crypt(sentence));
     }
+    @Test
+    public void shouldHaveProperDecryptedResult() {
+        String sentence = "--.. -.-- --. -- ..- -. -     ..-. .-. . ..- -..     .--- . ... -     -. .- --.. -.-- .-- .- -. -.--     --- .--- -.-. . --     .--. ... -.-- -.-. .... --- .- -. .- .-.. .. --.. -.--";
+        String expected = "zygmunt freud jest nazywany ojcem psychoanalizy";
+
+        ICrypter decrypter = new MorseCrypter();
+        assertEquals(expected, decrypter.decrypt(sentence));
+    }
+    @Test
+    public void shouldHaveProperDecryptedResult1() {
+        String sentence = ". -- .- -. -.-. -.-- .--. .- -.-. .--- .-     -.- --- -... .. . -     -- .. .- .-.. .-     ... .-- --- .---     .-. --- --.. -.- .-- .. -     .--     .---- ----. ....- .....";
+        String expected = "emancypacja kobiet miala swoj rozkwit w 1945";
+
+        ICrypter decrypter = new MorseCrypter();
+        assertEquals(expected, decrypter.decrypt(sentence));
+    }
 }
