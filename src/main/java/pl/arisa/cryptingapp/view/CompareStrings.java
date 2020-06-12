59bdb4f3bca9ad1ecd5a6d1f432c3e454319d3a5
AFormela <aleksandra_formela@vp.pl>
2020-06-12 22:55:46 +0200

    corrections after change of ui and compare view implementation

--- src/main/java/pl/arisa/cryptingapp/view/CompareStrings.java
+++ src/main/java/pl/arisa/cryptingapp/view/CompareStrings.java
@@ -1,14 +1,19 @@
 package pl.arisa.cryptingapp.view;
 
+import pl.arisa.cryptingapp.crypter.Crypter;
+import pl.arisa.cryptingapp.crypter.ICrypter;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CompareStrings extends JFrame {
     private JButton compareTextsButton;
     private JPanel CompareText;
-    private JTextPane textPane1;
-    private JTextPane textPane2;
-    private JPanel FirstText;
-    private JPanel SecondText;
+    private JTextPane SecondText;
+    private JTextPane FirstText;
+    private JPanel FirstTextPanel;
+    private JPanel SecondTextPanel;
     private JTextPane ResultPane2;
     private JPanel ResultPanel;
     private JButton returnToMenuButton;
@@ -23,5 +28,23 @@ public class CompareStrings extends JFrame {
         setContentPane(CompareText);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
+
+        compareTextsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ICrypter crypter = new Crypter();
+                String result = crypter.compareCryptedValues(FirstText.getText(), SecondText.getText());
+                ResultPane2.setText(result);
+            }
+        });
+
+        returnToMenuButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainMenu menu = new MainMenu();
+                menu.setVisible(true);
+                dispose();
+            }
+        });
     }
 }
